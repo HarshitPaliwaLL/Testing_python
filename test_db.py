f2678a3c2aae3806426bd08aef4a58c5e2a9b5cd
HarshitPaliwaLL <harshitpaliwal1811@gmail.com>
2023-09-07 19:39:41 +0530

    harshit commit

---
+++ test_db.py
@@ -0,0 +1,34 @@
+import bd
+import pytest
+from decimal import Decimal
+import datetime
+
+
+expected_output = [
+    [
+        (1, 'shivdutt', Decimal('500000.20'), datetime.date(2001, 3, 6)),
+        (2, 'mikku', Decimal('400000.20'), datetime.date(2001, 5, 27)),
+        (3, 'dewansh sharma ', Decimal('2500000.20'), datetime.date(2001, 9, 4)),
+        (4, 'sahil matta  ', Decimal('410000.20'), datetime.date(2001, 5, 29))
+    ]
+]
+
+def test_data():
+    actual_output = bd.db_fetching()
+    emp = []
+    emp.append(actual_output)
+    print(expected_output)
+    if expected_output != actual_output:
+        print("Expected Output:")
+        for row in expected_output:
+            print(row)
+        print("Actual Output:")
+        for row in actual_output:
+            print(row)
+    assert expected_output == emp
+
+def main():
+    test_data()
+
+if __name__ == "__main__":
+    main()
