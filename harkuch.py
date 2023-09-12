7e61b79b1ed9d72a1cc82125f853dce3ede4cc8e
HarshitPaliwaLL <harshitpaliwal1811@gmail.com>
2023-09-12 21:28:32 +0530

    hello

---
+++ harkuch.py
@@ -0,0 +1,9 @@
+class mera_nam_harshit:
+    def __init__(self):
+        name= input("enter your name ")
+        sername = input("enter you sername ")
+        print(f"hello ny name is {name} and my sername is {sername}".format(name=name,sername=sername))
+
+
+yash = mera_nam_harshit()
+
